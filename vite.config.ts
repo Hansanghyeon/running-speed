a68146bde4abcb41519fbc9bf0232f940a65ec07
Hansanghyeon <999@hyeon.pro>
2024-11-12 16:32:43 +0000

    feat: deploy domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/",
+  base: "/runspeed/",
   plugins: [react()],
   resolve: {
     alias: {
